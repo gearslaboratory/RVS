7b29168d59eb01142e098bf0b4aeae3d51b3d09d
ncinglis <ninglis@unr.edu>
2024-06-14 12:27:51 -0700

    succession h

--- librvs/Succession/SuccessionDriver.h
+++ librvs/Succession/SuccessionDriver.h
@@ -62,7 +62,7 @@ namespace Succession
 		void calcConfidence(int year, double biomass, double* lower, double* upper, double* level);
 		double calc_s2b(double* lnNPP);
 		//double calc_s2b(string* grp_id, double* lnNDVI, double* lnPPT);
-		double** matrix_mult(double** A, int aRow, int aCol);
+		double** matrix_mult(double** A, int aRow, int aCol, double** B, int bRow, int bCol);
 		double** matrix_trans(double** A, int aRow, int aCol);
 		double** generate_dummy_variables(int index, double* lnNPP);
 
